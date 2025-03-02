50e282803dbb6151f32c09a4e2b59eb01e399336
Anastasia Gridneva <anastasia.gridneva@mail.ru>
2025-03-02 11:25:13 +0300

    feature: added pagination

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,20 +1,23 @@
 import {getPostsApiThunk, selectPosts} from "../../slices/postsSlice.ts";
 import {useDispatch, useSelector} from "react-redux";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {AppDispatch} from "../../servicies/store.ts";
 import {PostsList} from "../PostsList/PostsList.tsx";
+import {Pagination} from "@mui/material";
 
 function App() {
   const dispatch = useDispatch<AppDispatch>();
+  const [page, setPage] = useState<number>(1);
   const posts = useSelector(selectPosts);
   useEffect(() => {
-    dispatch(getPostsApiThunk())
-  }, [dispatch])
-  console.log(posts)
+    dispatch(getPostsApiThunk(page))
+  }, [dispatch, page])
+
   return (
-      <>
+      <div>
         <PostsList posts={posts}/>
-      </>
+          <Pagination count={10} onChange={(event, value)=> setPage(value)} page={page}/>
+      </div>
   )
 }
 
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,8 +3,8 @@ import {IPost} from "./types.ts";
 const checkResponse = <T>(res: Response): Promise<T> =>
     res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
 
-export const getPosts = (): Promise<IPost[]> =>
-    fetch('https://jsonplaceholder.typicode.com/posts')
+export const getPosts = (page: number): Promise<IPost[]> =>
+    fetch(`https://jsonplaceholder.typicode.com/posts?_page=${page}`)
         .then((response) => checkResponse<IPost[]>(response))
         .then((data: IPost[]) => {
             if (data) {
