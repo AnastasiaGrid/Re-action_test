6013a2e2d52016f8111d0aff8f24101743ba8286
Anastasia Gridneva <anastasia.gridneva@mail.ru>
2025-03-03 12:18:35 +0300

    feature: added request error messages

--- src/components/PostsList/PostsList.tsx
+++ src/components/PostsList/PostsList.tsx
@@ -1,14 +1,23 @@
-import {JSX} from "react";
 import {PostItem} from "../PostItem/PostItem.tsx";
 import {IPost} from "../../utils/types.ts";
 import styles from './PostsList.module.scss'
+import {CircularProgress} from "@mui/material";
+import {useSelector} from "react-redux";
+import {selectError} from "../../slices/postsSlice.ts";
 
-export function PostsList({posts}: {posts: IPost[]}): JSX.Element {
+interface IPostsListProps {
+    posts: IPost[];
+    loading: boolean;
+}
+
+export function PostsList({posts,loading}: IPostsListProps) {
+    const error = useSelector(selectError) || 'Title not found. Please make sure you entered the entire post title'
+    if (loading) return <CircularProgress className={styles.loader}/>
     return (
         <div>
             {posts.length > 0 ? <ul className={styles.list}>
                 {posts.map((post, index) => <PostItem key={index} post={post}/>)}
-            </ul> : <p className={styles.error}>Title not found. Please make sure you entered the entire post title</p>}
+            </ul> : <p className={styles.error}>{error}</p>}
         </div>
     )
 }
